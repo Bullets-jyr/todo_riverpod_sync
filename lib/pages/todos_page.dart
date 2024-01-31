2bf9e9085be3ab3382aaa25d372cefd7139f99ab
Bullets-jyr <jyr@bullets.co.kr>
2024-01-31 16:22:58 +0900

    104. TodoHeader widget

--- lib/pages/todos_page.dart
+++ lib/pages/todos_page.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 
+import 'widgets/todo_header.dart';
+
 class TodosPage extends StatelessWidget {
   const TodosPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Center(
-        child: Text('TODOS'),
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            TodoHeader(),
+          ],
+        ),
       ),
     );
   }
