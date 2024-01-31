2196d826d924053ff58247d398e35ba065ed9c9d
Bullets-jyr <jyr@bullets.co.kr>
2024-01-31 18:33:01 +0900

    110. TodoItem widget - 2

--- lib/pages/widgets/todo_item.dart
+++ lib/pages/widgets/todo_item.dart
@@ -15,6 +15,14 @@ class TodoItem extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return ListTile(
+      onTap: () {
+        showDialog(
+          context: context,
+          builder: (context) {
+            return ConfirmEditDialog(todo: todo);
+          },
+        );
+      },
       leading: Checkbox(
         value: todo.completed,
         onChanged: (bool? checked) {
@@ -53,6 +61,70 @@ class TodoItem extends ConsumerWidget {
         },
         icon: const Icon(Icons.delete),
       ),
+    );
+  }
+}
+
+class ConfirmEditDialog extends ConsumerStatefulWidget {
+  final Todo todo;
+
+  const ConfirmEditDialog({
+    super.key,
+    required this.todo,
+  });
+
+  @override
+  ConsumerState createState() => _ConfirmEditDialogState();
+}
+
+class _ConfirmEditDialogState extends ConsumerState<ConfirmEditDialog> {
+  late final TextEditingController textController;
+  bool error = false;
+
+  @override
+  void initState() {
+    super.initState();
+    textController = TextEditingController(text: widget.todo.desc);
+  }
+
+  @override
+  void dispose() {
+    textController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text('Edit Todo'),
+      content: TextField(
+        controller: textController,
+        autofocus: true,
+        decoration: InputDecoration(
+          errorText: error ? 'Value cannot be empty' : null,
+        ),
+      ),
+      actions: [
+        TextButton(
+          onPressed: () => Navigator.pop(context),
+          child: const Text('CANCEL'),
+        ),
+        TextButton(
+          onPressed: () {
+            error = textController.text.isEmpty ? true : false;
+            if (error) {
+              setState(() {});
+            } else {
+              ref.read(todoListProvider.notifier).editTodo(
+                    widget.todo.id,
+                    textController.text,
+                  );
+              Navigator.pop(context);
+            }
+          },
+          child: const Text('EDIT'),
+        ),
+      ],
     );
   }
 }
